fa851c52a9acbc791f243458ffa860d5cc625255
Mayur Kode <mayurkode7@gmail.com>
2023-03-12 14:13:25 +0530

    css and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import "./App.css"
+import NotFound from "./images/404.png"
+import Clear from "./images/clear.png"
+import Cloud from "./images/cloud.png"
+import Mist from "./images/mist.png"
+import Rain from "./images/rain.png"
+import Snow from "./images/snow.png"
 
 function App() {
   return (
-    <div className='App'>
-      <div className='container'>
-        <div className='search-box'>
-          <i
-            className='fa-solid fa-location-dot
-          '
-          ></i>
-        </div>
-        <input type='text' placeholder='Enter your locatin' />
+    <div className='container'>
+      <div className='search-box'>
+        <i className='fa-solid fa-location-dot'></i>
+        <input type='text' placeholder='Enter your location' />
         <button className='fa-solid fa-magnifying-glass'></button>
       </div>
 
       <div className='not-found'>
-        <img src='images/404.png' alt='Page not found!' />
+        <img src={NotFound} alt='Page not found!' />
         <p>Oops! Invalid Location!</p>
       </div>
 
-      <div className='wether-box'>
+      <div className='weather-box'>
         <img src='' alt='' />
-        <p className='temperature'></p>
+        <p className='temperature'>
+          <span></span>
+        </p>
         <p className='description'></p>
       </div>
 
